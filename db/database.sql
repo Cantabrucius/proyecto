d980b51664a1855c8a87c49bcddc074b48779026
danigutidam <39051056+danigutidam@users.noreply.github.com>
2018-05-23 10:04:24 +0200

    Version preliminar de la estructura de la base de datos.

---
+++ db/database.sql
@@ -0,0 +1,118 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 23-05-2018 a las 09:59:37
+-- Versión del servidor: 10.1.31-MariaDB
+-- Versión de PHP: 7.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `eventos`
+--
+
+CREATE TABLE `eventos` (
+  `idevento` int(20) UNSIGNED NOT NULL,
+  `titulo` varchar(20) COLLATE latin1_spanish_ci NOT NULL,
+  `fechainicio` date NOT NULL,
+  `fechafin` date NOT NULL,
+  `horainicio` time NOT NULL,
+  `horafin` time NOT NULL,
+  `localidad` enum('Santander','Torrelavega','Maliaño','Astillero','Comillas','Santoña') COLLATE latin1_spanish_ci NOT NULL,
+  `recinto` varchar(20) COLLATE latin1_spanish_ci NOT NULL,
+  `precio` decimal(19,2) NOT NULL,
+  `imagen` varchar(100) COLLATE latin1_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `seguimiento`
+--
+
+CREATE TABLE `seguimiento` (
+  `nick` varchar(20) COLLATE latin1_spanish_ci NOT NULL,
+  `idevento` int(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `nombre` varchar(30) COLLATE latin1_spanish_ci NOT NULL,
+  `apellidos` varchar(50) COLLATE latin1_spanish_ci NOT NULL,
+  `nick` varchar(10) COLLATE latin1_spanish_ci NOT NULL,
+  `email` varchar(20) COLLATE latin1_spanish_ci NOT NULL,
+  `telefono` varchar(9) COLLATE latin1_spanish_ci NOT NULL,
+  `localidad` enum('Santander','Torrelavega','Maliaño','Astillero','Comillas','Santoña') COLLATE latin1_spanish_ci NOT NULL,
+  `foto` varchar(30) COLLATE latin1_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `eventos`
+--
+ALTER TABLE `eventos`
+  ADD PRIMARY KEY (`idevento`);
+
+--
+-- Indices de la tabla `seguimiento`
+--
+ALTER TABLE `seguimiento`
+  ADD KEY `nick` (`nick`),
+  ADD KEY `idevento` (`idevento`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`nick`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `eventos`
+--
+ALTER TABLE `eventos`
+  MODIFY `idevento` int(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `seguimiento`
+--
+ALTER TABLE `seguimiento`
+  ADD CONSTRAINT `seguimiento_ibfk_1` FOREIGN KEY (`nick`) REFERENCES `usuarios` (`nick`),
+  ADD CONSTRAINT `seguimiento_ibfk_2` FOREIGN KEY (`idevento`) REFERENCES `eventos` (`idevento`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
